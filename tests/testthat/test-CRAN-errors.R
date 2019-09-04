2247c12d69a266f80c97a4f1cfbe828ecb0dd5fa
Toby Dylan Hocking <tdhock5@gmail.com>
2019-09-04 09:57:23 -0700

    rename pkg dir to simulat pkg not available

--- tests/testthat/test-CRAN-errors.R
+++ tests/testthat/test-CRAN-errors.R
@@ -4,8 +4,25 @@ context("errors")
 
 test_that("error for unknown engine", {
   expect_error({
-    capture_first_vec("foo", bar="bax", engine="sars")
+    capture_first_vec("foo", bar="foo", engine="sars")
   }, "engine must be character string")
+})
+
+expect_error_engine_pkg <- function(engine, pkg){
+  if(isNamespaceLoaded(pkg))unloadNamespace(pkg)
+  pkg.dir <- system.file(package=pkg)
+  new.dir <- paste0(pkg.dir, "_tmp")
+  file.rename(pkg.dir, new.dir)
+  expect_error({
+    capture_first_vec("foo", bar="foo", engine=engine)
+  }, pkg)
+  file.rename(new.dir, pkg.dir)
+}
+test_that("error tells user to install re2r when RE2 not available", {
+  expect_error_engine_pkg("RE2", "re2r")
+})
+test_that("error tells user to install stringi when ICU not available", {
+  expect_error_engine_pkg("ICU", "stringi")
 })
 
 for(engine in c("PCRE", "RE2", "ICU")){
