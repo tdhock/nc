8531fea5abde88646ecbdea8d8bdfd04f4404e00
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2020-02-27 12:31:56 -0700

    alternatives tests pass

--- R/alternatives.R
+++ R/alternatives.R
@@ -1,10 +1,16 @@
-alternatives <- function(...){
+alternatives <- function
+### Make a pattern that matches one of the specified alternatives.
+(...
+### Each argument is a different alternative pattern.
+){
   in.list <- list(...)
-  stopifnot(1 < length(in.list))
+  if(length(in.list) < 2){
+    stop("alternatives should have at least two arguments")
+  }
   out.list <- in.list[1]
   for(i in 2:length(in.list)){
-    out.list[[length(out.list)+1L]] <- "|"
-    out.list[[length(out.list)+1L]] <- in.list[[i]]
+    out.list <- c(out.list, c("|", in.list[i]))
   }
   out.list
+### Pattern list.
 }
