bbc49eca6fd1d0d0d6f839085e879cd66a940a6b
Toby Dylan Hocking <tdhock5@gmail.com>
2020-09-29 20:53:48 -0700

    test abbrev err

--- tests/testthat/test-CRAN-melt.R
+++ tests/testthat/test-CRAN-melt.R
@@ -29,6 +29,15 @@ test_engines("error for fun always returning NA", {
   fixed=TRUE)
 })
 
+test_engines("error for fun always returning NA abbrev cols", {
+  subject <- data.frame(t(1:99))
+  expect_error({
+    capture_melt_single(subject, part=".*", as.numeric)
+  },
+  "need to change type conversion function(s), which should return at least one non-NA, but are always returning NA, even though regex matched 99 column(s): X1,X2,X3,X4,X5,X6,X7,X8,X9,X10,...,X89,X90,X91,X92,X93,X94,X95,X96,X97,X98,X99",
+  fixed=TRUE)
+})
+
 test_engines("possessive (.*+) error(RE2) or OK(others)", {
   posmatch <- function(){
     capture_melt_single(
